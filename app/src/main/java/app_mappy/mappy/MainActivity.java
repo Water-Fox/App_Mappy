5c040624e955d1da6bee6da52e3349e7e434d91c
Ski-lab <sk01na03@gmail.com>
2014-12-15 19:20:41 +0900

    Google Mapの表示実装

--- app/src/main/java/app_mappy/mappy/MainActivity.java
+++ app/src/main/java/app_mappy/mappy/MainActivity.java
@@ -1,10 +1,9 @@
 package app_mappy.mappy;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,9 +11,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
 
 
@@ -39,4 +35,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
